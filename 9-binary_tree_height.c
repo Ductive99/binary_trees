6e51566b296d3dbe160e79b86f7f1cc2f7c5461d
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-03 17:50:59 +0100

    Added travesal functions and height function

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure
+ *
+ * Return: height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	size_t left = 0, right = 0;
+
+	if (tree->left)
+		left = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right = binary_tree_height(tree->right) + 1;
+
+	return ((right > left) ? right : left);
+}
